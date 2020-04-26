5f71bbbc89eb668139a72d2704e74631b47a9d6a
Naura9 <63914381+Naura9@users.noreply.github.com>
2020-04-26 14:29:52 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="fontawesome/css/all.min.css">
+
+        <title>Hello, world!</title>
+  </head>
+  
+  <body>
+        <div class="card">
+            <div class="info">
+                <div class="name">Naura Haidar</div>
+                <div class="prof">Siswa</div>
+                <div class="divider"></div>
+                <div class="bio">
+                    Hai semuanya, saya sekarang bersekolah di SMK Telkom Malang kelas X RPL 7. Website ini digunakan untuk pengecekan virus covid-19,
+                    agar tidak terjadi penyebaran secara meluas. Stay Healthy and Stay at Home.
+                </div>
+                <button> <a href="http://localhost/corona/login/index.php" class="text-light">LOGIN </a></button>
+            </div>
+            <div class="photo"></div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="admin.js"></script>
+   </body>
+</html>
